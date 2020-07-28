504a3763579e0584065d239e279d9232941e07b6
qztbl <qztbl@qzt.com>
2020-07-28 17:38:48 +0800

    modify android applicationId

--- example/android/app/src/main/java/com/example/svcode/MainActivity.java
+++ example/android/app/src/main/java/com/example/svcode/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.example.svcode;
 
 import com.facebook.react.ReactActivity;
 
--- example/android/app/src/main/java/com/example/svcode/MainApplication.java
+++ example/android/app/src/main/java/com/example/svcode/MainApplication.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.example.svcode;
 
 import android.app.Application;
 import android.content.Context;
